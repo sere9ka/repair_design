58b317051e94459aa042e6a9e76491abbebe350d
sere9ka <s.kovylov@zhiet.ru>
2020-05-16 13:27:42 +0300

    refact: change Javascript to JQuery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector ('.modal');
   const modalBtn = document.querySelectorAll ('[data-toggle=modal]');
@@ -30,3 +31,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
   };
 
 })
+*/
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle="modal"]'),
+      closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+});
