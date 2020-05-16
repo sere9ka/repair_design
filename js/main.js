af8d964e65d960dd0295da39dd6c27303499bd3b
sere9ka <s.kovylov@zhiet.ru>
2020-05-16 12:58:36 +0300

    feat: add function for close modal window key Escape

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector ('.modal');
   const modalBtn = document.querySelectorAll ('[data-toggle=modal]');
+  let modalClick = document.querySelector ('.modal__dialog')
   const closeBtn = document.querySelector ('.modal__close');
+  const closeKey = document.addEventListener ('keydown', function (event) {
+    if (event.code == 'Escape') {
+      modal.classList.remove("modal--visible");
+    } else {
+      console.log('Не смог сделать второе условие...');
+    }
+  });
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
   }
@@ -10,6 +18,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
     element.addEventListener('click', switchModal);
   });
 
-  closeBtn.addEventListener('click', switchModal);
+
+  const modalVisible = document.querySelector ('.modal--visible');
+  modalClick.addEventListener('click', switchModal);
+  document.onclick = () => {
+    if ( event.target.className != '.modal__dialog' && modalVisible == 1) {
+      switchModal();
+    } else {
+      console.log('false');
+    }
+  };
 
 })
